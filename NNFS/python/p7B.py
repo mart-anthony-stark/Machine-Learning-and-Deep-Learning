b83b67844965670349ce1072a51bc0638c44fb7d
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-02 21:59:18 +0800

    chore(nnfs): add Categorical Cross-Entropy

---
+++ NNFS/python/p7B.py
@@ -0,0 +1,16 @@
+#  Calculating Loss with Categorical Cross-Entropy
+'''
+solving for x
+e**x = b
+'''
+import numpy as np
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+
+b = 5.2
+
+print(np.log(b))
+print(math.e**1.6486586255873816)
