49c4ff1b813d3368b62a14c927135fc8d13b90a9
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-02 21:59:17 +0800

    chore(nnfs): add Categorical Cross-Entropy

---
+++ NNFS/python/p7A.py
@@ -0,0 +1,12 @@
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1]) * target_output[1] +
+         math.log(softmax_output[2]) * target_output[2])
+
+print(loss)
+print(-math.log(0.7))
+print(-math.log(0.5))
