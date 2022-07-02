9848b4c591e0ca97c1719b298d0b8aa12219c575
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-02 20:41:30 +0800

    chore(nnfs): add Softmax activation

---
+++ NNFS/python/p6.py
@@ -0,0 +1,58 @@
+# Softmax activation
+import numpy as np
+import nnfs
+
+# y = e^x
+# eulers = 2.718
+nnfs.init()
+
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4,
+                        points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+
+X, y = spiral_data(100, 3)
+
+
+class DenseLayer:
+    def __init__(self, inputs_n, neurons_n):
+        self.weights = 0.10 * np.random.randn(inputs_n, neurons_n)
+        self.biases = np.zeros((1, neurons_n))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class ActivationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+dense1 = DenseLayer(2, 3)
+activation1 = ActivationReLU()
+
+dense2 = DenseLayer(3, 3)
+activation2 = ActivationSoftmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output[:5])
