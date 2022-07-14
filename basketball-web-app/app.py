1b6b40c04b5008dac2ad167c9f143f5efcc0cfad
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-14 21:17:29 +0800

    add config

--- basketball-web-app/app.py
+++ basketball-web-app/app.py
@@ -22,8 +22,12 @@ def load_data(year):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     html = pd.read_html(url, header = 0)
     df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    raw = df.drop(df[df['Age'] == 'Age'].index) # Deletes repeating headers in content
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
 playerstats = load_data(selected_year)
+
+# Sidebar - Team selection
+sorted_unique_team = sorted(playerstats['Tm'].unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
