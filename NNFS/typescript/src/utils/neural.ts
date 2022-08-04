c3798cad292c8704838af1b5cf20cea9592059be
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-04 19:58:32 +0800

    add activation

--- NNFS/typescript/src/utils/neural.ts
+++ NNFS/typescript/src/utils/neural.ts
@@ -1,17 +1,19 @@
 import { uid } from "uid";
 
 /**
- * Basic neuron component in neural network
+ * Neuron - Basic component in neural network where biases and weights are multiplied
  */
 export class NeuralPoint {
   readonly id: string = uid();
   readonly bias: number;
   readonly incoming: any;
-  readonly outgoing: any;
-  readonly _output: number; // f'(x)
-  readonly output: number; // f(x)
   readonly error: number; // E'(f(x))
-  connect: (neuron: any, weight: number) => void;
+
+  outgoing: any;
+  _output: number; // f'(x)
+  output: number; // f(x)
+  // connect: (neuron: any, weight: number) => void;
+  // activate: (input: any) => number;
 
   constructor() {
     this.id = uid();
@@ -30,14 +32,49 @@ export class NeuralPoint {
       neurons: {},
       weights: {},
     };
+  }
 
-    this.connect = (neuron, weight) => {
-      this.outgoing.neurons[neuron.id] = neuron;
-      neuron.incoming.neurons[this.id] = this;
+  connect(neuron: any, weight: number): void {
+    this.outgoing.neurons[neuron.id] = neuron;
+    neuron.incoming.neurons[this.id] = this;
 
-      // weight ∈ ℝ && -1 < weight < 1
-      this.outgoing.weights[neuron.id] = neuron.incoming.weights[this.id] =
-        weight == undefined ? Math.random() * 2 - 1 : weight;
-    };
+    // weight ∈ ℝ && -1 < weight < 1
+    this.outgoing.weights[neuron.id] = neuron.incoming.weights[this.id] =
+      weight == undefined ? Math.random() * 2 - 1 : weight;
+  }
+
+  activate(input: number): number {
+    const self = this;
+
+    function sigmoid(x: number) {
+      return 1 / (1 + Math.exp(-x));
+    } // f(x) = 1 / (1 + e^(-x))
+    function _sigmoid(x: number) {
+      return sigmoid(x) * (1 - sigmoid(x));
+    } // f'(x) = f(x) * (1 - f(x))
+
+    // Input Neurons
+    if (input) {
+      this._output = 1; // f'(x)
+      this.output = input; // f(x)
+    }
+    // Hidden/Output Neurons
+    else {
+      // Σ (x • w)
+      const sum = Object.keys(this.incoming.targets).reduce(function (
+        total,
+        target,
+        index
+      ) {
+        return (total +=
+          self.incoming.targets[target].output * self.incoming.weights[target]);
+      },
+      this.bias);
+
+      this._output = _sigmoid(sum); // f'(x)
+      this.output = sigmoid(sum); // f(x)
+    }
+
+    return this.output;
   }
 }
