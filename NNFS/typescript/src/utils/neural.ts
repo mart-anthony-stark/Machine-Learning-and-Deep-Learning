4b28076948b4c165ede25a15dee5e64e97e02654
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-04 19:47:29 +0800

    add connectfunction

--- NNFS/typescript/src/utils/neural.ts
+++ NNFS/typescript/src/utils/neural.ts
@@ -6,16 +6,38 @@ import { uid } from "uid";
 export class NeuralPoint {
   readonly id: string = uid();
   readonly bias: number;
-  readonly incoming:any;
-  readonly outgoing:any;
-  readonly _output:number; // f'(x)
-  readonly output:number; // f(x)
+  readonly incoming: any;
+  readonly outgoing: any;
+  readonly _output: number; // f'(x)
+  readonly output: number; // f(x)
   readonly error: number; // E'(f(x))
+  connect: (neuron: any, weight: number) => void;
 
   constructor() {
     this.id = uid();
 
     // bias ∈ ℝ && -1 < bias < 1
     this.bias = this.bias == undefined ? Math.random() * 2 - 1 : this.bias;
+
+    // Incoming Connections
+    this.incoming = {
+      neurons: {},
+      weights: {},
+    };
+
+    // Outgoing Connections
+    this.outgoing = {
+      neurons: {},
+      weights: {},
+    };
+
+    this.connect = (neuron, weight) => {
+      this.outgoing.neurons[neuron.id] = neuron;
+      neuron.incoming.neurons[this.id] = this;
+
+      // weight ∈ ℝ && -1 < weight < 1
+      this.outgoing.weights[neuron.id] = neuron.incoming.weights[this.id] =
+        weight == undefined ? Math.random() * 2 - 1 : weight;
+    };
   }
 }
